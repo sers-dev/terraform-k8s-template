a50350ddb7a559cfebfbe7c5ac621d59e4117342
Bobonium <bobonium@sers.dev>
2022-06-05 18:49:36 +0200

    added nonResourceUrls config to example

--- example/module/submodules/replaceMe/12_rbac.tf
+++ example/module/submodules/replaceMe/12_rbac.tf
@@ -2,18 +2,20 @@ locals {
   rbac = {
     clusterRoleRules = [
       #{
-      #  apiGroups     = [""]
-      #  resources     = [""]
-      #  verbs         = [""]
-      #  resourceNames = []
+      #  apiGroups       = [""]
+      #  resources       = [""]
+      #  verbs           = [""]
+      #  resourceNames   = []
+      #  nonResourceUrls = []
       #},
     ]
     roleRules = [
       #{
-      #  apiGroups     = [""]
-      #  resources     = [""]
-      #  verbs         = [""]
-      #  resourceNames = []
+      #  apiGroups       = [""]
+      #  resources       = [""]
+      #  verbs           = [""]
+      #  resourceNames   = []
+      #  nonResourceUrls = []
       #},
     ]
   }
